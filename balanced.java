1148ae140bc0726c81b261a1374315281ea1c17d
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-13 18:52:02 +0530

    Update balanced.java

--- balanced.java
+++ balanced.java
@@ -18,35 +18,46 @@ public class balanced
 			
 			for(int j=0;j<ss[i].length();j++)
 			{
-				int a=ss[i].charAt(j);
-				if(a>96&&a<123)
-					a=a-96;
-				else if(a>64&&a<91)
-					a=a-64;
-				
-				if(j==0)
+				if(Character.isLetter(ss[i].charAt(j)))
 				{
-					if(a<=13)
-						c='l';
-					if(a>=14&&a<=26)
-						c='u';
+						int a=ss[i].charAt(j);
+						if(a>96&&a<123)
+							a=a-96;
+						else if(a>64&&a<91)
+							a=a-64;
+						
+						if(j==0)
+						{
+							if(a<=13)
+								c='l';
+							if(a>=14&&a<=26)
+								c='u';
+						}
+						else if(c=='l')
+						{
+							c='u';
+							if(a<=13)
+								f=1;
+						}
+						else if(c=='u')
+						{
+							c='l';
+							if(a>=14&&a<=26)
+								f=1;
+						}
 				}
-				else if(c=='l')
+				else
 				{
-					c='u';
-					if(a<=13)
-						f=1;
+				f=2;
+				break;
 				}
-				else if(c=='u')
-				{
-					c='l';
-					if(a>=14&&a<=26)
-						f=1;
-				}
+					
 			}
 			
 			if(f==0)
 				System.out.println(ss[i]+" is balanced");
+			else if(f==2)
+				System.out.println("invalid");
 			else
 				System.out.println(ss[i]+" is not balanced");
 		}
