55599ac3efa6002f9f8ee30dd4f3c1fb26920ae3
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-13 19:46:21 +0530

    Update array.java

--- array.java
+++ array.java
@@ -6,6 +6,7 @@ public class array
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
+		//length of array
 		int n=sc.nextInt();
 		int[] a=new int[n];
 		int max=0;
