761750a952a268063795081d3b5c40d1c85dc20f
Aswiniraja <aswinirajaeee@gmail.com>
2016-12-30 22:53:14 +0530

    Create string_ascii.java

---
+++ string_ascii.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class string_ascii 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		String s=sc.next();
+		int sum=0;
+		
+		for(int i=1;i<s.length();i++)
+		{
+			sum=sum+((int)(s.charAt(i)-(int)(s.charAt(i-1))));
+		}
+
+		sum=sum-(int)(s.charAt(0));
+		System.out.println((char)sum);
+	}
+
+}
